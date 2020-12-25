6ae3651c13a6c626720ada85de2abe80783da2c0
Yashvirani <viraniyash2001@gmail.com>
2020-12-25 21:36:46 +0530

    commit

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const {isSignedIn,isAdmin,isAuthenticated} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
-const {getProductById,createProduct} = require("../controllers/product");
+const {getProductById,createProduct,getProduct,photo} = require("../controllers/product");
 
 router.param("userId",getUserById);
 router.param("productId",getProductById);
 
 
-router.post("/product/create/userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
+router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
+router.get("/product/:productId",getProduct);
+router.get("/product/photo:productId/",photo);
 
 module.exports = router;
