4c1710c603144befd826dd0f1304055db3d09a8a
Yashvirani <viraniyash2001@gmail.com>
2020-12-26 12:31:13 +0530

    commit

--- controllers/product.js
+++ controllers/product.js
@@ -85,11 +85,41 @@ exports.deleteProduct = (req,res) => {
             });
         };
         res.json({
-            message:"Deleted Successfully!"
+            message:"Deleted Successfully!",
+            deletedProduct
         });
     });
 };
 
 exports.updateProduct = (req,res) => {
-    
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req,(err,fields,file) => {
+        if(err){
+            return res.status(400).json({
+                error:"Problem with Image"
+            });
+        };
+        //product updation
+        
+        let product = req.product;
+        product = lodash.extend(product,fields);
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error:"File to be less than 3 Mb"
+                });
+            };
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contentType = file.photo.type;
+        }
+        product.save((err,product) => {
+            if(err){
+                return res.status.json({
+                    error:"Couldn't update product"
+                });
+            };
+            res.json(product);
+        });
+    });
 }
