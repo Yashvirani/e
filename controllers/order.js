362942793baaa2fd732ca5bb116a94e993c42660
Yashvirani <viraniyash2001@gmail.com>
2020-12-26 16:28:57 +0530

    commit

--- controllers/order.js
+++ controllers/order.js
@@ -11,5 +11,18 @@ exports.getOrderById = (req,res,next,id) => {
         };
         req.order = orderCorrespondingToId;
         next();
+    });
+};
+
+exports.createOrder = (req,res) => {
+    req.body.order.user = req.profile;
+    const order = new Order(req.body.order);
+    order.save((err,order) => {
+        if(err){
+            return res.status(400).json({
+                error:"Couldn't save Order!"
+            });
+        };
+        res.json(order);
     })
 }
